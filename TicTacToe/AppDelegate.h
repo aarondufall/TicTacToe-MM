54967d55afcdaba7936a1223cd0a2701710a2368
Aaron Dufall <aald212@gmail.com>
2014-03-14 00:12:22 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Aaron Dufall on 14/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Aaron Dufall on 14/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
